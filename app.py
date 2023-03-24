96c965f3d1b6d58c090c8f2ce70753b1a03b97f0
AndrewSon3611 <andrewson3611@gmail.com>
2023-03-23 22:17:58 -0400

    update1

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
-
-
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint, choice, sample
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "chickensarecool1234"
+debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def home_page():
@@ -16,10 +19,42 @@ def home_page():
     """
     return html
 
+@app.route('/form')
+def show_form():
+    return render_template("form.html")
+
+@app.route('/form-2')
+def show_form_2():
+    return render_template("form_2.html")
+
+COMPLIMENTS = ["Cool", "Clever", "Tenacious", "Awesome", "Pythonic"]
+
+@app.route('/greet')
+def get_greeting():
+    username = request.args["username"]
+    nice_thing = choice(COMPLIMENTS)
+    return render_template("greet.html", username=username, compliment=nice_thing)
+
+@app.route('/greet-2')
+def get_greeting_2():
+    username = request.args["username"]
+    wants_compliments = request.args.get("wants_compliments")
+    nice_things = sample(COMPLIMENTS, 3)
+    return render_template("greet_2.html", username=username, wants_compliments=wants_compliments, nice_things=nice_things)
+
 @app.route('/hello')
 def say_hello():
     # return "Hello there!"
     return render_template("hello.html")
+
+@app.route('/lucky')
+def lucky_number():
+    num = randint(1,20)
+    return render_template('lucky.html', lucky_num = num, msg="You are so lucky!")
+
+@app.route('/spell/<word>')
+def spell_word(word):
+    return render_template("spell_word.html", word=word.upper())
 
 @app.route('/goodbye')
 def say_bye():
